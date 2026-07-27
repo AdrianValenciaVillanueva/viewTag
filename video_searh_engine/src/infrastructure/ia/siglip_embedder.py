7f04ed81734302c07ae05e6771cea89217bf8786
Adrian Valencia <villanuevaadrian123@gmail.com>
2026-07-26 21:56:29 -0600

    feat(ai): implement SigLIPEmbedder strategy using Hugging Face

--- video_searh_engine/src/infrastructure/ia/siglip_embedder.py
+++ video_searh_engine/src/infrastructure/ia/siglip_embedder.py
@@ -1,7 +1,48 @@
-# Concrete implementation for embedding generation using SigLIP
-# Example:
-# from src.domain.interfaces.embedded import Embedder
-# class SigLIPEmbedder(Embedder):
-#     def embed(self, text: str) -> list[float]:
-#         # Implementation using SigLIP model
-#         pass
+import io
+import torch 
+from  typing import List
+from PIL import Image
+from transformers import AutoProcessor, AutoModel
+
+from src.domain.interfaces.embedder import VectorEmbedderInterface
+
+class SigLIPEmbedder(VectorEmbedderInterface):
+    """
+    Estrategia para crear vectores con SigLIP mediante hugging face
+    """
+    def __init__(self, model_name:str = "google/siglip-base-patch16-224"):
+        #usar gpu
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        #cargar modelador de imagenes y un modelo preentrenado
+        self.processor = AutoProcessor.from_pretrained(model_name)
+        self.model = AutoModel.from_pretrained(model_name).to(self.device)
+    
+    def embed_image(self, image_bytes:bytes) -> List[float]:
+        """convierte los bytes de una imagen en una lista de numeros embed"""
+        image = Image.open(io.BytesIO(image_bytes))
+
+        #procesar la imagen 
+        #pasamos la imagen a tensor con torch
+        inputs = self.processor(images=image, return_tensors="pt").to(self.device)
+        
+        with torch.no_grad():
+            image_features = self.model.get_image_features(**inputs)
+            #normalizamos el vector
+            image_features = image_features/image_features.norm(dim=1, keepdim=True)
+
+            #devolvemos el vector
+            return image_features.squeeze().cpu().tolist()
+        
+    def embed_text(self, text:str) -> List[float]:
+        """convierte un texto en una lista de numeros embed"""
+        #procesar el texto
+        inputs = self.processor(text=text, return_tensors="pt", padding=True).to(self.device)
+        
+        with torch.no_grad():
+            text_features = self.model.get_text_features(**inputs)
+            #normalizamos el vector
+            text_features = text_features/text_features.norm(dim=1, keepdim=True)
+
+            #devolvemos el vector
+            return text_features.squeeze().cpu().tolist()
