2d847965cf036aa275b09b031ed3542be6d47165
Adrian Valencia Villanueva <adrian.valencia5250@alumnos.udg.mx>
2026-08-24 12:47:11 -0600

    feat(repository): implement the vectr class and abstract methods

--- video_searh_engine/src/domain/interfaces/repository.py
+++ video_searh_engine/src/domain/interfaces/repository.py
@@ -1,14 +1,26 @@
 # Define abstract interfaces for data persistence
-# Example:
-# from abc import ABC, abstractmethod
-# from typing import List
-# class VideoRepository(ABC):
-#     @abstractmethod
-#     def save(self, video: 'Video'):
-#         pass
-#     @abstractmethod
-#     def find_by_id(self, video_id: str) -> 'Video':
-#         pass
-#     @abstractmethod
-#     def find_all(self) -> List['Video']:
-#         pass
+from abc import ABC, abstractmethod
+from typing import List
+from video_searh_engine.src.domain.models import SearchResult
+
+class VectorRepositoryInterface(ABC):
+
+    @abstractmethod
+    def save_vectors(
+        self,
+        video_name: str,
+        frame_ids: List[str],
+        timestamps: List[float],
+        vectors: List[float]
+    ) -> None:
+        """guarda un conjunto de vectores y sus metadatos en la base de datos"""
+        pass
+
+    @abstractmethod
+    def search_similar(
+        self,
+        query_vector: List[float],
+        limit: int = 5
+    ) -> List[SearchResult]:
+        """busca los vectores mas cercanos a un vector de consulta"""
+        pass
